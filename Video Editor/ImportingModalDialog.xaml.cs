390a3f5ec7455ae4540aa0733316ff9b6f98d76f
big-javascript-fan <wdeveloping9099@gmail.com>
2023-02-13 09:34:37 -0500

    init

---
+++ Video Editor/ImportingModalDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Video_Editor
+{
+    /// <summary>
+    /// Interaction logic for ImportingModalDialog.xaml
+    /// </summary>
+    public partial class ImportingModalDialog : UserControl
+    {
+        public ImportingModalDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void CancelImport_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
