e3c827b4a7c22802188590af6578a7a00cb0e44d
john1226 <devil.vergil1226@gmail.com>
2023-02-17 05:43:53 -0800

    Timeline modified

--- WpfTest/MainWindow.xaml.cs
+++ WpfTest/MainWindow.xaml.cs
@@ -4,12 +4,17 @@ using OpenCvSharp.Extensions;
 using System;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Video_Editor;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WpfTest
 {
@@ -21,9 +26,10 @@ namespace WpfTest
         private VideoCapture _capture;
         private Thread videoThread;
         private bool _run = false, _threadClose = false, _fullscreen = false, _altPressed = false;
-        private int speedCount = 1000, clipInterval = 10;
-        private double clipWidth;
+        private int speedCount = 1000, startPos, endPos;
+        private double clipWidth, cutlinePosX;
         private string winName = "fullscreen";
+        private System.Windows.Point _positionInBlock;
 
         public MainWindow()
         {
@@ -62,55 +68,82 @@ namespace WpfTest
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
             if (openFileDialog.ShowDialog() == true)
             {
-                m_toolbar.Visibility = Visibility.Hidden;
-                mediaEditWindow.Visibility = Visibility.Visible;
-                mediaInputWindow.Visibility = Visibility.Hidden;
 
-                mediaInputTimeline.Visibility = Visibility.Hidden;
-                mediaEditTimeline.Visibility = Visibility.Visible;
+                BgGrid.Children.Add(new ImportingModalDialog());
+                DispatcherTimer time = new DispatcherTimer();
+                time.Interval = TimeSpan.FromSeconds(1);
+                time.Start();
+                time.Tick += delegate
+                {
 
-                string _videoFile = openFileDialog.FileName;
-                _capture = new VideoCapture(_videoFile);
+                    // Import the video file
+                    m_toolbar.Visibility = Visibility.Hidden;
+                    mediaEditWindow.Visibility = Visibility.Visible;
+                    mediaInputWindow.Visibility = Visibility.Hidden;
 
-                TimeLabel2.Content = GetFormatTime(_capture.FrameCount);
-                TimeSlider.Maximum = _capture.FrameCount;
+                    mediaInputTimeline.Visibility = Visibility.Hidden;
+                    mediaEditTimeline.Visibility = Visibility.Visible;
 
-                clipWidth = 50 * _capture.FrameWidth / _capture.FrameHeight;
-                InitClip();
+                    string _videoFile = openFileDialog.FileName;
+                    _capture = new VideoCapture(_videoFile);
 
-                videoThread = new Thread(PlayVideo);
-                videoThread.Start();
+                    TimeLabel2.Content = GetFormatTime(_capture.FrameCount);
+                    TimeSlider.Maximum = _capture.FrameCount;
+
+                    clipWidth = 50 * _capture.FrameWidth / _capture.FrameHeight;
+                    startPos = 0;
+                    endPos = _capture.FrameCount;
+                    InitClip();
+
+                    BgGrid.Children.Clear();
+                    time.Stop();
+
+                    videoThread = new Thread(PlayVideo);
+                    videoThread.Start();
+
+                    _run = true;
+                    Thread.Sleep(10);
+                    _run = false;
+                };
             }
         }
+
         private void InitClip()
         {
             this.Dispatcher.Invoke((Action)(() =>
             {
-                if (ClipStack == null) return;
                 ClipStack.Children.Clear();
-                TimeStack.Children.Clear();
-                int pos, prevPos = _capture.PosFrames;
-                for (pos = 0; pos < _capture.FrameCount; pos += clipInterval)
+
+                int cnt = (int)(ClipStack.ActualWidth / clipWidth);
+                double clipInterval = (double)(endPos - startPos) / cnt;
+                int prevPos = _capture.PosFrames;
+                for ( int i = 0; i < cnt; i++ )
                 {
-                    _capture.PosFrames = pos;
+                    _capture.PosFrames = (int)(i * clipInterval + startPos);
                     Mat _image = new Mat();
                     _capture.Read(_image);
                     var bmpClip = BitmapConverter.ToBitmap(_image);
                     System.Windows.Controls.Image img = new System.Windows.Controls.Image();
                     img.Source = BitmapToImageSource(bmpClip);
                     img.Height = 50;
+                    img.Width = ClipStack.ActualWidth / cnt;
                     ClipStack.Children.Add(img);
                 }
                 _capture.PosFrames = prevPos;
 
-                for (pos = 0; pos <= _capture.FrameCount; pos += (int)(_capture.Fps))
-                {
-                    System.Windows.Controls.Label _label = new System.Windows.Controls.Label();
-                    _label.Content = GetFormatTime(pos);
-                    _label.Width = clipWidth / clipInterval * _capture.Fps;
-                    _label.Foreground = System.Windows.Media.Brushes.White;
-                    TimeStack.Children.Add(_label);
-                }
+                TimeStart.Content = GetFormatTime(startPos);
+                TimeEnd.Content = GetFormatTime(endPos);
+            }));
+        }
+
+        private void ShowClip()
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                Mat _image = new Mat();
+                _capture.Read(_image);
+                if (_image.Empty()) return;
+                VideoViewer.Source = BitmapToImageSource(BitmapConverter.ToBitmap(_image));
             }));
         }
 
@@ -174,6 +207,15 @@ namespace WpfTest
             }
         }
 
+        private void SetTimeLinePosition(double zoomRate, double posX)
+        {
+            int frameCount = endPos - startPos;
+            startPos += (int)(frameCount * zoomRate * posX / ClipStack.ActualWidth);
+            endPos -= (int)(frameCount * zoomRate * (ClipStack.ActualWidth - posX) / ClipStack.ActualWidth);
+            if (startPos < 0) startPos = 0;
+            if (endPos > _capture.FrameCount) endPos = _capture.FrameCount;
+        }
+
         private void Export_Click(object sender, RoutedEventArgs e)
         {
         }
@@ -196,24 +238,25 @@ namespace WpfTest
 
         private void ZoomOut(object sender, RoutedEventArgs e)
         {
-            clipInterval += 10;
-            if (clipInterval > 100) clipInterval = 100;
-            ZoomSlider.Value = 110 - clipInterval;
-            InitClip();
+            if (ZoomSlider.Value == ZoomSlider.Minimum) return;
+            double zoomRate = ZoomSlider.Value - 1;
+            if (zoomRate < ZoomSlider.Minimum) zoomRate = ZoomSlider.Minimum;
+            ZoomSlider.Value = zoomRate;
         }
 
         private void ZoomIn(object sender, RoutedEventArgs e)
         {
-            clipInterval -= 10;
-            if (clipInterval < 10)  clipInterval = 10;
-            ZoomSlider.Value = 110 - clipInterval;
-            InitClip();
+            if (ZoomSlider.Value == ZoomSlider.Maximum) return;
+            double zoomRate = ZoomSlider.Value + 1;
+            if (zoomRate > ZoomSlider.Maximum) zoomRate = ZoomSlider.Maximum;
+            ZoomSlider.Value = zoomRate;
         }
 
         private void ZoomSliderChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            clipInterval = 110 - (int)e.NewValue;
-            InitClip();
+            if (ClipStack == null) return;
+            SetTimeLinePosition((e.NewValue - e.OldValue) / e.OldValue, cutlinePosX);
+            (new Thread(InitClip)).Start();
         }
 
         private void FormKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -222,6 +265,50 @@ namespace WpfTest
             {
                 _altPressed = true;
             }
+        }
+
+        private void CutButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // when the mouse is down, get the position within the current control. (so the control top/left doesn't move to the mouse position)
+            _positionInBlock = Mouse.GetPosition(CutButton);
+
+            // capture the mouse (so the mouse move events are still triggered (even when the mouse is not above the control)
+            CutButton.CaptureMouse();
+            _run = false;
+        }
+
+        private void CutButtonMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (CutButton.IsMouseCaptured)
+            {
+                // get the parent container
+                var container = VisualTreeHelper.GetParent(CutButton) as UIElement;
+
+                if (container == null)
+                    return;
+
+                // get the position within the container
+                var mousePosition = e.GetPosition(container);
+
+                // move the user control.
+                double x = mousePosition.X - _positionInBlock.X + 10;
+                if (x < 0) x = 0;
+                if (x > ClipStack.ActualWidth) x = ClipStack.ActualWidth;
+                cutlinePosX = x;
+                CutButton.RenderTransform = new TranslateTransform(x, 0);
+                CutLine.RenderTransform = new TranslateTransform(x, 0);
+                int pos = (int)(startPos + (endPos - startPos) * x / ClipStack.ActualWidth);
+                CutLabel.RenderTransform = new TranslateTransform(x, 0);
+                CutLabel.Content = GetFormatTime(pos);
+                _capture.PosFrames = pos;
+                (new Thread(ShowClip)).Start();
+            }
+        }
+
+        private void CutButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // release this control.
+            CutButton.ReleaseMouseCapture();
         }
 
         private void FormKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
@@ -236,24 +323,32 @@ namespace WpfTest
         {
             if (_altPressed)
             {
-                int _zoomRate = 3;
                 if (e.Delta > 0)
                 {
-                    if (clipInterval + _zoomRate < _capture.Fps)
-                        clipInterval += _zoomRate;
+                    ZoomIn(sender, null);
                 }
                 else
                 {
-                    if (clipInterval - _zoomRate > 0)
-                        clipInterval -= _zoomRate;
+                    ZoomOut(sender, null);
                 }
-                Thread clipThread = new Thread(InitClip);
-                clipThread.Start();
             }
             else
             {
-                ClipScroll.ScrollToHorizontalOffset(ClipScroll.HorizontalOffset - e.Delta);
-                TimeScroll.ScrollToHorizontalOffset(TimeScroll.HorizontalOffset - e.Delta);
+                if (e.Delta > 0 && startPos == 0 || e.Delta < 0 && endPos == _capture.FrameCount) return;
+                int s = e.Delta > 0 ? -1 : 1;
+                startPos += s * (endPos - startPos) / 10;
+                endPos += s * (endPos - startPos) / 10;
+                if (startPos < 0)
+                {
+                    endPos -= startPos;
+                    startPos = 0;
+                }
+                if (endPos > _capture.FrameCount)
+                {
+                    startPos -= endPos - _capture.FrameCount;
+                    endPos = _capture.FrameCount;
+                }
+                (new Thread(InitClip)).Start();
             }
         }
 
@@ -262,7 +357,6 @@ namespace WpfTest
 //             Cv2.NamedWindow(winName, WindowFlags.Normal);
 //             Cv2.SetWindowProperty(winName, WindowPropertyFlags.Fullscreen, 1.0);
 //             _fullscreen = true;
-            
         }
 
         private void SliderChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
